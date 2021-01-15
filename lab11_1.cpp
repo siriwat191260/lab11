729e762f6f0109c36a052532b9945a7e2198639a
siriwat191260 <pan-191260@hotmail.com>
2021-01-15 16:01:40 +0700

    finish

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,16 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(long long int);
 
 int main(){
-    int x;
-    cin >> x;
+    long long int x = 50;
     cout << fibonacci(x);
     return 0; 
 }
 
-int fibonacci(int n){
+long long int fibonacci(long long int n){
     if(n == 1) return 1;
     if(n == 0) return 0;
     return fibonacci(n-1) + fibonacci(n-2);
